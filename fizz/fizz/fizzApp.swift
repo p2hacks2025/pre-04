49c7349431a5087d582fee65001a08e4fdcf3420
eich4826 <yruiyilang@gmail.com>
2025-12-18 03:41:21 +0900

    feat: XcodeとVScodeの連携

---
+++ fizz/fizz/fizzApp.swift
@@ -0,0 +1,32 @@
+//
+//  fizzApp.swift
+//  fizz
+//
+//  Created by technolo-gia on 2025/12/18.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct fizzApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ fizz/fizzTests/fizzTests.swift
@@ -0,0 +1,16 @@
+//
+//  fizzTests.swift
+//  fizzTests
+//
+//  Created by technolo-gia on 2025/12/18.
+//
+
+import Testing
+
+struct fizzTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
